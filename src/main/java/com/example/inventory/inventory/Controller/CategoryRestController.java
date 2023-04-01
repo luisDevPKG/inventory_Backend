1d9ada6f165f0f63f53033c5522043095978ef93
lmosquera <lmosquera.sinergia@gmail.com>
2023-04-01 16:51:42 -0500

    feat: se agrega configuracion de cors en el controller

--- src/main/java/com/example/inventory/inventory/Controller/CategoryRestController.java
+++ src/main/java/com/example/inventory/inventory/Controller/CategoryRestController.java
@@ -2,6 +2,7 @@ package com.example.inventory.inventory.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.example.inventory.inventory.Model.Category;
 import com.example.inventory.inventory.Reponse.CategoryResponseRest;
 import com.example.inventory.inventory.Service.CategoryService;
 
+@CrossOrigin(origins = {"http://localhost:4200"}) // configuracion CORS 4200 puerto por defecto angular
 @RestController
 @RequestMapping("/api/v1") // genero la uri general del servicio
 public class CategoryRestController {
